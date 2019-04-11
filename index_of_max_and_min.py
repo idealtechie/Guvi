94aaff822073a5a3d01340865e8bb75efb4f9635
Adarsh <48917401+idealtechie@users.noreply.github.com>
2019-04-11 22:06:04 +0530

    Create index_of_max_and_min.py

---
+++ index_of_max_and_min.py
@@ -0,0 +1,14 @@
+a=input()
+a=a.split()
+k=[]
+
+for i in a:
+    k.append(int(i))
+x=max(k)
+y=min(k)
+for i in range(0,len(k)):
+    if(k[i]==x):
+        mx=i+1
+    if(k[i]==y):
+        mn=i+1
+print(str(mn)+" "+str(mx))
