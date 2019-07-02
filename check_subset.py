4ffbeeb77210f6d082fb389c417bf9b96345a3d3
Adarsh <48917401+idealtechie@users.noreply.github.com>
2019-07-02 16:30:26 +0530

    Create check_subset.py

---
+++ check_subset.py
@@ -0,0 +1,14 @@
+a=input()
+b=input()
+b=b.split()
+c=input()
+c=c.split()
+flag=1
+for i in c:
+    if(i not in b):
+        flag=0
+        break
+if(flag=="0"):
+    print("No")
+else:
+    print("YES")
